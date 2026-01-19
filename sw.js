6d7209a858b5fc9ce64acd19a0ffd0d389f4af7e
pkiachriposia-bit <pkiachriposia@gmail.com>
2026-01-19 19:22:00 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  'icon.png',      // 512x512
+  'mobile.png',    // 192x192
+  'sw.js',
+  'boundary.geojson',
+  'building.geojson',
+  'main_roads.geojson',
+  'pitch.geojson',
+  'roads.geojson',
+  'https://unpkg.com/leaflet/dist/leaflet.css',
+  'https://unpkg.com/leaflet/dist/leaflet.js',
+  'https://unpkg.com/@turf/turf/turf.min.js'
+];
